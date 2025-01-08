d3d6cf3a5834ff35af511123b53ba97d3cf42a9d
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-01-08 18:17:10 +0400

    initial

--- app/api/webhooks/stripe/route.tsx
+++ app/api/webhooks/stripe/route.tsx
@@ -1,3 +1,33 @@
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+import { sendPurchaseReceipt } from "@/emails";
+import Order from "@/lib/db/models/order.model"; // Make sure this is your Order model
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2024-12-18.acacia",
+});
+
+export async function POST(req: NextRequest) {
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      await req.text(),
+      req.headers.get("stripe-signature") as string,
+      process.env.STRIPE_WEBHOOK_SECRET as string
+    );
+  } catch (err) {
+    console.error("Webhook signature verification failed:", err);
+    return new NextResponse("Webhook Error", { status: 400 });
+  }
+
+  console.log("Webhook received:", event.type);
+
+  const { type, data } = event;
+  processEventAsync(type, data); // Offload to async processing
+  return new NextResponse("Received", { status: 200 });
+}
+
 async function processEventAsync(type: string, data: Stripe.Event.Data) {
   if (type === "charge.succeeded") {
     const charge = data.object as Stripe.Charge; // Cast to Stripe.Charge object
