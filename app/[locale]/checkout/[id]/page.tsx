b0b7a31c0cc86e440a4f896191052d69be0b39fa
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-01-08 16:48:39 +0400

    initial

--- app/[locale]/checkout/[id]/page.tsx
+++ app/[locale]/checkout/[id]/page.tsx
@@ -1,6 +1,5 @@
 import { notFound } from "next/navigation";
 import React from "react";
-
 import { auth } from "@/auth";
 import { getOrderById } from "@/lib/actions/order.actions";
 import PaymentForm from "./payment-form";
@@ -10,30 +9,31 @@ export const metadata = {
   title: "Payment",
 };
 
-const CheckoutPaymentPage = async (props: {
-  params: Promise<{
-    id: string;
-  }>;
+const CheckoutPaymentPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
 }) => {
-  const params = await props.params;
-
-  const { id } = params;
+  const { id } = await params; // Await the params promise
 
   const order = await getOrderById(id);
   if (!order) notFound();
 
   const session = await auth();
-
   let client_secret = null;
+
   if (order.paymentMethod === "Stripe" && !order.isPaid) {
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+      apiVersion: "2024-12-18.acacia",
+    });
+
     const paymentIntent = await stripe.paymentIntents.create({
       amount: Math.round(order.totalPrice * 100),
       currency: "USD",
-      metadata: { orderId: order._id.toString() }, // Ensure _id is converted to a string
+      metadata: { orderId: order._id.toString() },
     });
-    console.log("Metadata sent to Stripe:", { orderId: order._id.toString() });
 
+    console.log("Metadata sent to Stripe:", { orderId: order._id.toString() });
     client_secret = paymentIntent.client_secret;
   }
 
@@ -42,7 +42,7 @@ const CheckoutPaymentPage = async (props: {
       order={order}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
       clientSecret={client_secret}
-      isAdmin={session?.user?.role === "Admin" || false}
+      isAdmin={session?.user?.role === "Admin"}
     />
   );
 };
--- app/[locale]/checkout/[id]/stripe-payment-success/page.tsx
+++ app/[locale]/checkout/[id]/stripe-payment-success/page.tsx
@@ -1,41 +1,44 @@
 import Link from "next/link";
 import { notFound, redirect } from "next/navigation";
 import Stripe from "stripe";
+
 import { Button } from "@/components/ui/button";
 import { getOrderById } from "@/lib/actions/order.actions";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2024-12-18.acacia",
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
-export default async function SuccessPage({
-  params,
-  searchParams,
-}: {
-  params: { id: string };
-  searchParams: { payment_intent: string };
+export default async function SuccessPage(props: {
+  params: Promise<{
+    id: string;
+  }>;
+  searchParams: Promise<{ payment_intent: string }>;
 }) {
-  const { id } = params;
-  const { payment_intent } = searchParams;
+  const params = await props.params;
 
+  const { id } = params;
+
+  const searchParams = await props.searchParams;
   const order = await getOrderById(id);
   if (!order) notFound();
 
-  const paymentIntent = await stripe.paymentIntents.retrieve(payment_intent);
-  const metadata: Record<string, string> = paymentIntent.metadata || {};
-
-  if (metadata.orderId !== order._id.toString()) notFound();
+  const paymentIntent = await stripe.paymentIntents.retrieve(
+    searchParams.payment_intent
+  );
+  if (
+    paymentIntent.metadata.orderId == null ||
+    paymentIntent.metadata.orderId !== order._id.toString()
+  )
+    return notFound();
 
   const isSuccess = paymentIntent.status === "succeeded";
-  if (!isSuccess) redirect(`/checkout/${id}`);
-
+  if (!isSuccess) return redirect(`/checkout/${id}`);
   return (
     <div className="max-w-4xl w-full mx-auto space-y-8">
-      <div className="flex flex-col gap-6 items-center">
+      <div className="flex flex-col gap-6 items-center ">
         <h1 className="font-bold text-2xl lg:text-3xl">
           Thanks for your purchase
         </h1>
-        <p>We are now processing your order.</p>
+        <div>We are now processing your order.</div>
         <Button asChild>
           <Link href={`/account/orders/${id}`}>View order</Link>
         </Button>
