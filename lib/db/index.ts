3a0927f5908c3abf99751aa6b0593aae4228ddcf
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-01-09 11:42:49 +0400

    initial

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,19 +1,20 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-// Cache for MongoDB connection
-const cached = (global as any).mongoose || { conn: null, promise: null };
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const cached = (global as any).mongoose || { conn: null, promise: null }
 
-export const connectToDatabase = async (MONGODB_URI = process.env.MONGODB_URI) => {
-  if (cached.conn) return cached.conn;
+export const connectToDatabase = async (
+  MONGODB_URI = process.env.MONGODB_URI
+) => {
+  if (cached.conn) return cached.conn
 
-  if (!MONGODB_URI) throw new Error("MONGODB_URI is missing");
+  if (!MONGODB_URI) throw new Error('MONGODB_URI is missing')
 
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      serverSelectionTimeoutMS: 30000, // 30 seconds
-    });
-  }
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI,{
+    serverSelectionTimeoutMS: 15000,
+ } )
 
-  cached.conn = await cached.promise;
-  return cached.conn;
-};
+  cached.conn = await cached.promise
+
+  return cached.conn
+}
