beee33cca9ad89f620185a03121c98c07afb37ed
ShwetaBhattacharjee <shwetabhattacharje@gmail.com>
2025-01-09 12:49:43 +0400

    initial

--- lib/db/models/order.model.ts
+++ lib/db/models/order.model.ts
@@ -1,24 +1,24 @@
-import { IOrderInput } from "@/types";
-import { Document, Model, model, models, Schema } from "mongoose";
+import { IOrderInput } from '@/types'
+import { Document, Model, model, models, Schema } from 'mongoose'
 
 export interface IOrder extends Document, IOrderInput {
-  _id: string;
-  createdAt: Date;
-  updatedAt: Date;
+  _id: string
+  createdAt: Date
+  updatedAt: Date
 }
 
 const orderSchema = new Schema<IOrder>(
   {
     user: {
       type: Schema.Types.ObjectId as unknown as typeof String,
-      ref: "User",
+      ref: 'User',
       required: true,
     },
     items: [
       {
         product: {
           type: Schema.Types.ObjectId,
-          ref: "Product",
+          ref: 'Product',
           required: true,
         },
         clientId: { type: String, required: true },
@@ -53,12 +53,14 @@ const orderSchema = new Schema<IOrder>(
     paidAt: { type: Date },
     isDelivered: { type: Boolean, required: true, default: false },
     deliveredAt: { type: Date },
+    createdAt: { type: Date, default: Date.now },
   },
   {
     timestamps: true,
   }
-);
+)
 
-const Order = (models.Order as Model<IOrder>) || model<IOrder>("Order", orderSchema);
+const Order =
+  (models.Order as Model<IOrder>) || model<IOrder>('Order', orderSchema)
 
-export default Order;
+export default Order
